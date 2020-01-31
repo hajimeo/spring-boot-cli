bc194daab2d0016ae6691eb92414255ddc6d4670
hajime <github@hajigle.com>
2020-01-31 18:01:50 +1000

    using my p12 file.

--- HttpsApplication/HttpsApplication.groovy
+++ HttpsApplication/HttpsApplication.groovy
@@ -4,7 +4,12 @@
 As per https://www.baeldung.com/spring-boot-cli
   Created ./config/application.properties
 Also, created ./resources/javadevjournal.p12 as this should be included in the classpath
-  and using server.ssl.key-store=resources/javadevjournal.p12
+  and using server.ssl.key-store=resources/standalone.localdomain.p12
+
+How to override keystore:
+  java -jar HttpsApplication.jar --spring.config.location=./new_config/application.properties
+Didn't work (maybe because application.properties was good?):
+  #java -Djavax.net.ssl.keyStore=./resources/javadevjournal.p12 -Djavax.net.ssl.keyStorePassword=changeit -jar HttpsApplication.jar
  */
 
 @RestController
